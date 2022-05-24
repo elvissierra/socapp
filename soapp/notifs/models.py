d61caa6ec3bb249cb33c4e58e3397f1ac2115b72
elvis <elvissierra3@gmail.com>
2022-05-23 22:03:38 -0500

    ./

--- soapp/notifs/models.py
+++ soapp/notifs/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Notifs(models.Model):
     # id = models.AutoField(primary_key = True)
     content = models.TextField()
+    image = models.FileField(upload_to="images/", blank=True, null=True)
