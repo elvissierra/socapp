c65ff863585557f2db5b23b249d832ffaa3421d8
essn0w <72325201+elvissierra@users.noreply.github.com>
2022-05-24 07:37:53 -0500

    Delete soapp/notifs directory

--- soapp/notifs/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Notifs(models.Model):
-    # id = models.AutoField(primary_key = True)
-    content = models.TextField()
-    image = models.FileField(upload_to="images/", blank=True, null=True)
