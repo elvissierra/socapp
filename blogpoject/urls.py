0382e134b2ee8176eb61952b97879386b24e62b3
elvis <elvissierra3@gmail.com>
2022-05-28 17:47:17 -0500

    proj fix..

--- blogpoject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from soshapp.views import home_view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_view),
-]
